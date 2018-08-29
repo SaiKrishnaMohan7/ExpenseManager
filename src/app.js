e29adcd08b3d5d729321cf5eff6222fcd0c761fd
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2018-08-29 12:54:32 -0400

    React Ruoter init setup, exact path match, changes to webpack devServer config to accommodate 404 return index.html for 404

--- src/app.js
+++ src/app.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 
 import './styles/styles.scss'
 
-ReactDOM.render(<p>Boilerplate</p>, document.getElementById('root'));
+const ExpenseDashboard = () => {
+    return (
+    <div>
+        this i from the dashboard component
+    </div>        
+
+    );
+};
+
+const AddExpense = () => {
+    return (
+    <div>
+        this i from the AddExpense component
+    </div>        
+
+    );
+};
+
+const EditExpense = () => {
+    return (
+        <p>From Edit Expense</p>
+    );
+};
+
+const Help = () => {
+    return (<p>Help Page</p>);
+};
+
+
+const routes = (
+    <BrowserRouter>
+        <Fragment>
+            <Route exact={true} path="/" component={ExpenseDashboard} />
+            <Route path="/create" component={AddExpense}/>
+            <Route path="/edit" component={EditExpense} />
+            <Route path="/help" component={Help} />
+        </Fragment>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('root'));
