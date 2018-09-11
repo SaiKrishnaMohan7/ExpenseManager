a56f3d2c3fb51e5ab9066e5664d03b05acf630ad
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2018-09-10 21:20:50 -0400

    Stuck

---
+++ src/components/ExpenseListItem.js
@@ -0,0 +1,19 @@
+import React, {Fragment} from 'react';
+import { connect } from 'react-redux';
+
+import removeExpense from '../actions/expensesActionGen';
+
+const ExpenseListItem = ({id, description, amount, createdAt, dispatch}) => {
+  return (
+    <Fragment>
+      <h3>{description}</h3>
+      <p>{amount} - {createdAt}</p>
+      <button onClick={() => {dispatch(removeExpense(id));}}>
+          Remove
+      </button>
+    </Fragment>
+  );
+};
+
+// using connect() without args passes dispatch as a prop to the connected component
+export default connect()(ExpenseListItem);
