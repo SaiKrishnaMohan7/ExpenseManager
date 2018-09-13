af6a92bedc34ebe379af59a453fdba23d16463fc
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2018-09-12 20:12:12 -0400

    Added Sorting by date and amount and update to Notes

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import { setTextFilter } from '../actions/filtersActionGen';
+import { setTextFilter, sortByDate, sortByAmount } from '../actions/filtersActionGen';
 
 const ExpenseListFilters =  (props) => {
   return  (
@@ -8,6 +8,20 @@ const ExpenseListFilters =  (props) => {
       <input type="text" value={props.filters.text} onChange={(e) => {
         props.dispatch(setTextFilter(e.target.value));
       }}/>
+      <select 
+        value={props.filters.sortBy}
+        onChange={(e) => {
+          let selected = e.target.value;
+          
+          if (selected === 'date')
+            props.dispatch(sortByDate());
+          if(selected === 'amount')
+            props.dispatch(sortByAmount())
+        }}
+      >
+        <option value="date">Date</option>
+        <option value="amount">Amount</option>
+      </select>
     </div>
   );
 };
