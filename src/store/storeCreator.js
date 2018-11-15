5e3cf343635028883c5f142b377227598d7860e9
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2018-11-14 20:10:54 -0500

    Added redux devTools

--- src/store/storeCreator.js
+++ src/store/storeCreator.js
@@ -7,8 +7,8 @@ const storeCreator = () => {
 	const store = createStore(
 		combineReducers({
 		expenses: expensesReducer,
-		filters: filtersReducer
-	}));
+		filters: filtersReducer,
+	}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 	return store;
 };
