626d616aa8e238bf5d949feb916966107356e5ef
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2019-04-19 17:48:01 -0400

    small chnages

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -10,7 +10,9 @@ const ExpenseListItem = ({id, description, amount, createdAt}) => {
       <h3>
         <Link to={`/edit/${id}`}>{description}</Link>
       </h3>
-      <p>{numeral(amount / 100).format('$0,0.00')} - {moment(createdAt).format('Do MMMM, YYYY')}</p>
+      <p>
+        {numeral(amount / 100).format('$0,0.00')} - {moment(createdAt).format('Do MMMM, YYYY')}
+      </p>
     </Fragment>
   );
 };
