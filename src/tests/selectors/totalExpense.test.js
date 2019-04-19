10ffa5fce675c2e1e50a8cc7dddba563c1e28354
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2019-04-19 19:05:58 -0400

    Added a new selector to get total of all expenses

---
+++ src/tests/selectors/totalExpense.test.js
@@ -0,0 +1,22 @@
+import totalExpense from './../../selectors/totalExpense';
+import { expenses } from '../testData/testData';
+
+test('should return the total of all expenses', () => {
+  expect(totalExpense(expenses)).toEqual(20592);
+});
+
+test('should return 0 if expenses arr empty', () => {
+  const empty = [];
+
+  expect(totalExpense(empty)).toEqual(0);
+});
+
+test('should return 0 if expenses arr undefined', () => {
+  const empty = undefined;
+
+  expect(totalExpense(empty)).toEqual(0);
+});
+
+test('should return amount of one expense ir arr length is 1', () => {
+  expect(totalExpense([expenses[0]])).toEqual(20000);
+});
