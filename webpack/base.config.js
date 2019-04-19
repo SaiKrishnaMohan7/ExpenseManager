fc71409838706f90fb9b9832d1ea9f81e3a02576
SaiKrishnaMohan7 <m.saikrishna.msk@gmail.com>
2019-04-19 15:06:20 -0400

    Added HMR to devServer configuration

--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,23 +1,41 @@
 const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 	entry: './src/app.js',
 	output: {
-			path: path.join(__dirname, '../public'),
-			filename: 'bundle.js'
+		filename: 'bundle.js',
+		path: path.join(__dirname, '../public')
 	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "styles.css"
+		}),
+	],
 	module: {
-			rules: [{
-					loader: 'babel-loader',
-					test: /\.js$/,
-					exclude: /node_modules/
-			}, {
-					use: [
-							'style-loader',
-							'css-loader',
-							'sass-loader'
-					],
-					test: /\.s?css$/
-			}]
+		rules: [
+			{
+				loader: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			},
+			{
+				test: /\.s?css$/,
+				use: [
+					'style-loader',
+					MiniCssExtractPlugin.loader,
+					{
+						loader: "css-loader",
+						options: {
+							minimize: true,
+							sourceMap: true
+						}
+					},
+					{
+						loader: "sass-loader"
+					}
+				]
+			},
+		]
 	}
 };
